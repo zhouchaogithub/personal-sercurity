4c9476e77c79a60792fd25b180c78e9a0243b55f
zhouchao <zhouchaoit@sina.com>
2018-11-22 14:06:26 +0800

    项目架构

---
+++ personal-sercurity-demo/src/main/java/com/zc/security/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.zc.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: zhouchao
+ * @Date: 2018/11/22 13:30
+ * @Description:
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+**一、项目结构
+personal-sercurity：主模块
+personal-sercurity-core:核心业务逻辑
+personal-sercurity-browser:浏览器安全特定代码
+personal-sercurity-app：app相关特定代码
+personal-sercurity-demo：样例程序**
